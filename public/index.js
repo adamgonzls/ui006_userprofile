16a6a2132ad21a7d7aa69c2d2ef6ce92bfd378b5
Adam Gonzales <adamgonzales1@gmail.com>
2018-10-29 20:44:10 -0700

    add function 'loadUser'

--- public/index.js
+++ public/index.js
@@ -1,14 +1,29 @@
-document.addEventListener('DOMContentLoaded', function() {
-  console.log("Ready!");
-
-}, false);
-
 const requestURL = 'http://localhost:3000/asteam.json';
 const request = new XMLHttpRequest();
 request.open('GET', requestURL);
 request.responseType = 'json';
 request.send();
 request.onload = function() {
-  var team = request.response;
-  console.log(team);
+  const data = request.response;
+  loadUser(data);
 }
+
+function loadUser(data) {
+  const teamMemberImage = document.querySelector('.user-pic img');
+  const teamMemberName = document.querySelector('.user-info .name');
+  const teamMemberTitle = document.querySelector('.user-info .title');
+  const teamMemberEmail = document.querySelector('.contact-info .email');
+  const teamMemberPhone = document.querySelector('.contact-info .phone');
+  console.log(data)
+  // document.addEventListener('DOMContentLoaded', function() {
+    // userImage
+    teamMemberImage.src = data.team[0].image;
+    teamMemberImage.alt = data.team[0].name;
+    teamMemberName.innerHTML = data.team[0].name;
+    teamMemberTitle.innerHTML = data.team[0].title;
+    teamMemberEmail.innerHTML = data.team[0].email;
+    teamMemberPhone.innerHTML = data.team[0].phone;
+
+  // }, false);
+}
+
